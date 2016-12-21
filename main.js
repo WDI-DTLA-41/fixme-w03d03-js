18a1773d394ede082aa07e11e29ef54b227534ba
Phil Lamplugh <phillip.lamplugh@gmail.com>
2016-12-21 13:40:14 -0800

    add fixme

---
+++ main.js
@@ -0,0 +1,101 @@
+// Model
+var grid = [
+  [{clicks: 0}, {clicks: 0}, {clicks: 0}],
+  [{clicks: 0}, {clicks: 0}, {clicks: 0}],
+  [{clicks: 0}, {clicks: 0}, {clicks: 0}]
+];
+
+var markGrid = function(row, col) {
+  if (!grid[row]) return false;
+  grid[row][col].clicks += 1;
+  return true;
+}
+
+// View
+var $grid;
+var $markBtn;
+var $row;
+var $col;
+
+var render = function() {
+  $grid.innerHTML = renderGrid(grid);
+}
+
+var renderGrid = function(grid){
+  var html = '';
+  grid.forEach(function(row) {
+    html += renderRow(row);
+  })
+  return html;
+}
+
+var renderRow = function(row){
+  var html = '<div class="row">';
+  row.forEach(function(col){
+    html += renderCol(col);
+  });
+  html += '</div>';
+  return html;
+}
+
+var renderCol = function(col) {
+  var face = getFace(col.clicks);
+  var html = '<div class="col">' + face + '</div>';
+  return html;
+}
+
+var getFace = function(number) {
+  switch (number) {
+  case 0:
+    face = '😶';
+    break;
+  case 1:
+    face = '😕';
+    break;
+  case 2:
+    face = '🙁';
+    break;
+  case 3:
+    face = '☹';
+    break;
+  default:
+    face = '🤢';
+    break;
+  }
+  return face;
+}
+
+
+var handleClick = function(event) {
+  var pos = getPosition();
+  markGrid(pos.row, pos.col);
+  clearPosition();
+  render();
+}
+
+var addEventListeners = function() {
+  $markBtn.addEventListener('click', handleClick)
+}
+
+var getPosition = function(){
+  var position = {
+    row: $row.value,
+    col: $col.value
+  }
+  return position;
+}
+
+var clearPosition = function() {
+  $row.value = '';
+  $col.value = '';
+}
+
+// http://stackoverflow.com/questions/4584373/difference-between-window-load-and-document-ready-functionshttp://stackoverflow.com/questions/4584373/difference-between-window-load-and-document-ready-functions
+window.onload = function(){
+  $grid = document.querySelector('.grid');
+  $markBtn = document.querySelector('#mark');
+  $row = document.querySelector('input[name=row]');
+  $col = document.querySelector('input[name=col]');
+  render();
+  addEventListeners();
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Fix Me!
+
+Start up a server and take a look at `index.html`
+
+When a user enters a row and column and clicks "Mark" the face at 
+that space should change. 
+
+Read through the accompanying code in `main.js`
+
+Can you add in jQuery and refactor the code to use jQuery?
+
+# Bonus
+
+Can you update the the code to include to a click event
+to mark faces?
+
+# Resources
+
+- http://api.jquery.com/on/
+- http://api.jquery.com/html/
+- https://api.jquery.com/ready/
+- https://api.jquery.com/data/
